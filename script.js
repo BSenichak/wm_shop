35b708345b51f1d139ec67b0147266b2d1fb0367
BSenichak <bsenk77@gmail.com>
2025-12-13 17:43:16 +0200

    я так багато додав, ви не повірите скільки, плотіть гроши

--- script.js
+++ script.js
@@ -9,27 +9,68 @@ function getProducts(){
         console.log(data)
         products = data
         drawProducts()
+        drawCart()
     })
 }
 
 getProducts()
+
 
 function drawProducts(){
     main.innerHTML = ""
     products.forEach(p=>{
         main.innerHTML += `
         <div class="product">
-            <h3>${p.name}</h3>
-            <h4>$${p.price}</h4>
-            <a href="/seller.html?user_id=${p.user_id}">Seller page</a>
+        <h3>${p.name}</h3>
+        <h4>$${p.price}</h4>
+        <a href="/seller.html?user_id=${p.user_id}">Seller page</a>
+        <button onclick="addProductToCart(${p.id})">Купити!</button>
         </div>
         `
     })
 }
 
 let cartButton = document.getElementById("cart")
+let cart = document.querySelector(".cart")
 
 let cartIsOpen = false
 cartButton.addEventListener("click", function(){
     cartIsOpen = !cartIsOpen
+    cart.style.display = cartIsOpen ? "flex" : "none"
 })
+
+let cartArray = []
+
+function addProductToCart(product_id){
+    console.log(product_id)
+    cartArray.push(product_id)
+    localStorage.setItem("cart", JSON.stringify(cartArray))
+    drawCart()
+}
+
+function drawCart(){
+    if (cartArray.length == 0) {
+        cart.innerHTML = "Тут як в Нікіти в голові"
+        return
+    }
+    let cartProducts = products.filter(p=>cartArray.indexOf(p.id) > -1)
+    cartProducts = cartProducts.map(p => ({
+        ...p,
+        count: cartArray.filter(prod=>prod == p.id).length
+    }))
+    console.log(cartProducts)
+    cart.innerHTML = cartProducts.map(p=>`<li>${p.name} | $${p.price} | X${p.count}</li>`).join("")
+    cart.innerHTML += "<button onclick='clearCart()'>🚮 До Нікіти додому</button>"
+    let total = 0
+    cartProducts.forEach(p=>total += p.count * p.price)
+    cart.innerHTML += `<p>summ: ${total}</p>`
+}
+
+function clearCart(){
+    cartArray.length = 0
+    localStorage.setItem("cart", "[]")
+    drawCart();
+}
+
+cartArray = JSON.parse(localStorage.getItem("cart")) || []
+drawCart()
